a33ef67af9eecc93ea280b952cceb9ea39b76d48
lomadapraveenreddy <lomadapraveenreddy@gmail.com>
2020-08-06 19:41:45 +0530

    added check of recevied packet

--- traceroute.py
+++ traceroute.py
@@ -1,13 +1,19 @@
 import socket
+import struct
 
-HOST = 'google.com'
+HOST = 'iris.nitk.ac.in'
 PORT = 5005 # some random port
+ICMP_STRUCTURE_FMT = '!bbHHh'
+ip_address = socket.gethostbyname(HOST)
+
 
 def send_udp_packet(ttl):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
         s.connect((HOST, PORT))
         s.send(b'UDP message!')
+
+
 
 def capture_icmp_packet(ttl):
     with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as icmp_soc:
@@ -16,13 +22,23 @@ def capture_icmp_packet(ttl):
 
         try:
             (msg, addr) = icmp_soc.recvfrom(1024)
+            type,code,_,_,_=struct.unpack(ICMP_STRUCTURE_FMT,msg[20:28])
+            print(struct.unpack(ICMP_STRUCTURE_FMT,msg[20:28]))
+            if not (type==11 and code==0):
+                print('ICMP received but not of packet sent')
         except socket.timeout:
             print('ICMP msg not sent back in 1 second')
         else:
-            print(addr[0])
+            if(type == 11 and code == 0):
+                if ip_address==addr[0]:
+                    print('destination reached ',end='')
+                    print(ip_address+' '+addr[0])
+                    exit(1)
+                else:
+                    print(ip_address+' '+addr[0])
 
 if __name__ == '__main__':
-    for ttl in range(1, 10):
+    for ttl in range(1, 200):
         send_udp_packet(ttl)
         capture_icmp_packet(ttl)
 
